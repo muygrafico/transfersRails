40aa2c7d255c106c125b28b456ac397ae1ac3e55
German Hernandez <muygrafico@gmail.com>
2015-04-01 16:20:06 -0500

    DRYing shit up :P

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,62 +1,37 @@
 class Transfer < ActiveRecord::Base
-  after_save :balance_on_create
-  after_destroy :balance_on_destroy
-  after_update :balance_on_update
-
+  after_save :change_balance_on_create
+  after_destroy :change_balance_on_destroy
+  after_update :change_balance_on_update
   belongs_to :sender, :class_name => 'User'
   belongs_to :receiver, :class_name => 'User'
-
   private
-
-  def balance_on_create
-    update_balance(:on_create)
+  def change_balance_on_create
+    change_balance(:on_create)
   end
-
-  def balance_on_destroy
-    update_balance(:on_destroy)
+  def change_balance_on_destroy
+    change_balance(:on_destroy)
   end
-
-  def balance_on_update
-    update_balance(:on_update)
+  def change_balance_on_update
+    change_balance(:on_update)
   end
-
-  def update_balance(on_action)
-    #cb Stand for Current Balance
-
-    sender_cb = self.sender.balance
-    reciever_cb = self.receiver.balance
-
+  def update_balance(balance_sender, balance_reciever)
     sender = User.find_by_id(self.sender.id)
     reciever = User.find_by_id(self.receiver.id)
-
+    sender.update_attributes(balance: balance_sender )
+    receiver.update_attributes(balance: balance_reciever )
+  end
+  def change_balance(on_action)
+    #cb Stand for Current Balance
+    sender_cb = self.sender.balance
+    reciever_cb = self.receiver.balance
     case on_action
       when :on_create
-
-        balance_sender = sender_cb - amount
-        balance_reciever = reciever_cb + amount
-        sender.update_attributes(balance: balance_sender )
-        reciever.update_attributes(balance: balance_reciever )
-
+      update_balance(sender_cb - amount, reciever_cb + amount)
       when :on_destroy
-
-        balance_sender = sender_cb + amount
-        balance_reciever = reciever_cb - amount
-        sender.update_attributes(balance: balance_sender )
-        reciever.update_attributes(balance: balance_reciever )
-
+      update_balance(sender_cb + amount,reciever_cb - amount)
       when :on_update
-
-        balance_sender = sender_cb + amount
-        balance_reciever = reciever_cb - amount
-        sender.update_attributes(balance: balance_sender )
-        reciever.update_attributes(balance: balance_reciever )
-
-        balance_sender = sender_cb - amount
-        balance_reciever = reciever_cb + amount
-        sender.update_attributes(balance: balance_sender )
-        reciever.update_attributes(balance: balance_reciever )
-    end
-
- end
-
+      update_balance(sender_cb + amount,reciever_cb - amount)
+      update_balance( sender_cb - amount, reciever_cb + amount)
+      end
+  end
 end
